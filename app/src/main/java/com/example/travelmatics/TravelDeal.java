9ed4db9b6f6294ea01a6383def24991572294e4f
Nafiu Lawal <nafiudanlawal@gmail.com>
2020-07-18 18:39:55 +0000

    Befor Module 5

--- app/src/main/java/com/example/travelmatics/TravelDeal.java
+++ app/src/main/java/com/example/travelmatics/TravelDeal.java
@@ -1,6 +1,9 @@
 package com.example.travelmatics;
 
-public class TravelDeal {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TravelDeal implements Parcelable {
     private String id;
     private String title;
     private String description;
@@ -15,6 +18,26 @@ public class TravelDeal {
         this.price = price;
         this.imageURL = imageURL;
     }
+
+    protected TravelDeal(Parcel in) {
+        id = in.readString();
+        title = in.readString();
+        description = in.readString();
+        price = in.readString();
+        imageURL = in.readString();
+    }
+
+    public static final Creator<TravelDeal> CREATOR = new Creator<TravelDeal>() {
+        @Override
+        public TravelDeal createFromParcel(Parcel in) {
+            return new TravelDeal(in);
+        }
+
+        @Override
+        public TravelDeal[] newArray(int size) {
+            return new TravelDeal[size];
+        }
+    };
 
     public String getId() {
         return id;
@@ -54,5 +77,19 @@ public class TravelDeal {
 
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeString(title);
+        parcel.writeString(description);
+        parcel.writeString(price);
+        parcel.writeString(imageURL);
     }
 }
